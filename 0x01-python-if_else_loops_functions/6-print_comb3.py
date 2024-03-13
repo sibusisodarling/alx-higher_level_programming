25d01d71c569b0917908957f50964d2f7ce4c6d5
sibusisodarling <sibusisotradestone@gmail.com>
2024-03-13 22:18:38 +0000

    A program that prints all possible combinations of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i+1, 10):
-        if i == 0:
-            print("{}{}, ".format(i, j), end="")
+        if i == 8:
+            print("{}{}".format(i, j))
         else:
-            print("{}{}, ".format(i, j), end="")
-print()       
+            print("{}{}, ".format(i, j), end="")       
